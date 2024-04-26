31804c41e428637f31d9b65ae9f4ac5bcd98530b
hakeemellis <114674803+hakeemellis@users.noreply.github.com>
2024-04-26 00:41:19 -0400

    Updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' }, // redirects "/" to where I want it to
     { path: 'home', component: HomeComponent }, // Loads Home Components
     { path: 'login', component: LoginComponent }, // Loads Login Components
+    { path: 'dashboard', component: DashboardComponent }, // Loads Login Components
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  constructor(private router: Router) {}
+
+  navigateToDashboard(): void {
+    this.router.navigate(['dashboard']);
+  }
 
 }
