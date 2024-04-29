77c444d87e496832b26e2791c17eec10caac5929
hakeemellis <114674803+hakeemellis@users.noreply.github.com>
2024-04-28 23:36:47 -0400

    Updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ChequingComponent } from './components/chequing/chequing.component';
+import { TransactionHistoryComponent } from './components/transaction-history/transaction-history.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' }, // redirects "/" to where I want it to
@@ -12,4 +13,5 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent }, // Loads Login Components
     { path: 'dashboard', component: DashboardComponent }, // Loads Login Components
     { path: 'chequing', component: ChequingComponent }, // Loads Chequing Components
+    { path: 'transactionhistory', component: TransactionHistoryComponent }, // Loads Transaction History Components
 ];
--- src/app/components/chequing/chequing.component.ts
+++ src/app/components/chequing/chequing.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { TransactionHistoryComponent } from '../transaction-history/transaction-history.component';
 
 @Component({
   selector: 'app-chequing',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, TransactionHistoryComponent],
   templateUrl: './chequing.component.html',
   styleUrl: './chequing.component.scss'
 })
-export class ChequingComponent {
+export class ChequingComponent implements OnInit {
+
+  balance: number = 0; // Have to give integer a balance to start | if balance? - it would've turned optional
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.balance = 1000; // Balance is then defined here
+  }
 
 }
