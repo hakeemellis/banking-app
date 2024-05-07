7ef788877d0f89dbb6ccb48f21986a8046d6f869
hakeemellis <114674803+hakeemellis@users.noreply.github.com>
2024-05-07 15:33:35 -0400

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,36 @@ export class AppComponent {
   // Dark Mode
   darkMode = false;
 
+  ngOnInit(): void {
+    if (typeof localStorage !== 'undefined') {
+      const storedDarkMode = localStorage.getItem('darkMode');
+      if (storedDarkMode !== null) {
+        this.darkMode = JSON.parse(storedDarkMode);
+        this.applyDarkMode();
+      }
+    } else {
+      // Show in terminal/server that localStorage is not available
+      console.error('localStorage is not available.');
+    }
+  }
+
+
+  toggleDarkMode(): void {
+    this.darkMode = !this.darkMode;
+    this.applyDarkMode();
+    // To store dark mode state in localStorage
+    localStorage.setItem('darkMode', JSON.stringify(this.darkMode));
+  }
+
+  public applyDarkMode(): void {
+    if (this.darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+
+  /* OG DARK MODE CODE
   toggleDarkMode(): void{
     this.darkMode = !this.darkMode; //this reverses the logic and makes it true
 
@@ -25,6 +55,6 @@ export class AppComponent {
       document.documentElement.classList.remove('dark') // if false, remove the class
     }
   }
-
+  */
 }
 
